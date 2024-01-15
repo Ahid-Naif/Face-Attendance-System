277edc288a70dcb5d1f224e746b61f1936232948
Ahid_Naif <ahedqaood19@gmail.com>
2024-01-16 01:28:21 +0300

    updates

--- face_attendance_system.py
+++ face_attendance_system.py
@@ -1,24 +1,27 @@
-import face_recognition
-import openpyxl
-import xlrd
-from datetime import date
-from datetime import datetime
-import time
-from imutils import paths
-import os
-import cv2
-import numpy as np
+import face_recognition # AI
+import openpyxl # Excel
+import xlrd # Excel
+from datetime import date # Date & time from operating system
+from datetime import datetime # Date & time from operating system
+import time # Time from processor clock
+from imutils import paths # to read folders & images
+import os # to read folders & images
+import cv2 # OpenCV for image processing
+import numpy as np # for image processing
 
 # Initialize some variables for both departments
-known_face_names = []
-known_face_ids = []
+known_face_names = [] # to store names in dataset
+known_face_ids = [] # to store face ids in dataset
 dataset_path = "dataset"
 
-# Dictionary to keep track of sign-ins
+# to keep track of sign-ins
 last_attend_times = {}
 last_leave_times = {}
 
-for person_name in os.listdir(dataset_path):
+## START --- load dataset ---
+for person_name in os.listdir(dataset_path): # read folders [Raghad, Remas]
+    # read files one by one
+    # dataset/Raghad
     person_folder = os.path.join(dataset_path, person_name)
 
     # Check if it's a directory
@@ -26,24 +29,28 @@ for person_name in os.listdir(dataset_path):
         # Get all image paths for the current person
         image_paths = list(paths.list_images(person_folder))
         # loop over the image paths
-        for image_path in image_paths:
+        for image_path in image_paths: # read images
             # extract the person name from the image path
             print("[INFO] processing images")
             # name = image_path.split(os.path.sep)[-2]
-
+            ######################################################################
             # Load a sample picture and learn how to recognize it.
             image = face_recognition.load_image_file(image_path)
+            #get face Id
             face_encodings = face_recognition.face_encodings(image)
-                
+            ######################################################################   
             # In some cases, there might be no face found in an image
+            # len = length
             if len(face_encodings) > 0:
                 face_encoding = face_encodings[0]
+                # store person name in known_face_names variable
                 known_face_names.append(person_name)
+                # store person face id in known_face_ids variable
                 known_face_ids.append(face_encoding)
                 print(f"Encoded face for {person_name}")
             else:
                 print(f"No face found in {image_path}")
-
+## END --- load dataset ---
 
 def getFrameAfterRecognition(frame):
     # Resize frame of video to 1/4 size for faster face recognition processing
@@ -55,6 +62,8 @@ def getFrameAfterRecognition(frame):
 
     # Only process every other frame of video to save time
     # Find all the faces and face encodings in the current frame of video
+    # 1. Face detection
+    # 2. Face recognition
     face_locations = face_recognition.face_locations(rgb_small_frame)
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
@@ -172,9 +181,9 @@ def can_leave(person):
     return True
 
 if __name__ == "__main__":
-    entrance_camera = cv2.VideoCapture(0)
-    exit_camera = cv2.VideoCapture(1)
-    while True:
+    entrance_camera = cv2.VideoCapture(1) # 1 -> port number
+    exit_camera = cv2.VideoCapture(2) # 2 -> port number
+    while True: # infinite loop
         # Grab a single frame of video
         _, entrance_frame = entrance_camera.read()
         _, exit_frame = exit_camera.read()
@@ -192,8 +201,9 @@ if __name__ == "__main__":
                 left(person)
                 print(f"Left: {person}")
 
+        # Display
         cv2.imshow('Entrance Video', entrance_frame)
-        cv2.imshow('Exit Video', exit_frame)
+        # cv2.imshow('Exit Video', exit_frame)
 
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -201,5 +211,5 @@ if __name__ == "__main__":
 
     # Release handle to the webcam
     entrance_camera.release()
-    # exit_camera.release()
+    exit_camera.release()
     cv2.destroyAllWindows()
